e6f1e8aa916f161e25ce5de19846040632f6fca7
Aaqil Shihab <aaqilsh@yahoo.com>
2021-10-05 22:17:12 +0530

    Refactored method

--- Image Recongition/image_recongition.py
+++ Image Recongition/image_recongition.py
@@ -4,10 +4,10 @@ import boto3
 
 def detect_label():
     client = boto3.client('rekognition', region_name='us-east-1')
-    with open('Image Recongition/got.jpg', 'rb') as image:
-        respons = client.detect_labels(
+    with open('Image Recongition/images/got.jpg', 'rb') as image:
+        response = client.detect_labels(
             Image={"Bytes": image.read()}, MaxLabels=10)
-        print(respons)
+    return response
 
 
 if __name__ == '__main__':
