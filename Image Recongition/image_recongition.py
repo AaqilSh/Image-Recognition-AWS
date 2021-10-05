9ca64b6c24199d37b3bd4434f741b5ae2c6f7849
Aaqil Shihab <aaqilsh@yahoo.com>
2021-10-05 22:12:47 +0530

    Removed bucket method

--- Image Recongition/image_recongition.py
+++ Image Recongition/image_recongition.py
@@ -4,19 +4,11 @@ import boto3
 
 def detect_label():
     client = boto3.client('rekognition', region_name='us-east-1')
-    with open(r'got.jpg', 'rb') as image:
+    with open('Image Recongition/got.jpg', 'rb') as image:
         respons = client.detect_labels(
             Image={"Bytes": image.read()}, MaxLabels=10)
-        # print('hello')
-        # print(respons)
-        print(client)
-
-
-def bucket():
-    client = boto3.client('s3', region_name='us-east-1')
-    client.create_bucket(Bucket='aaqil1-sample-bucket')
+        print(respons)
 
 
 if __name__ == '__main__':
     detect_label()
-    # bucket()
