3189fd7319936cff058c6c441d72adb34e6af5c1
Aaqil Shihab <aaqilsh@yahoo.com>
2021-10-06 20:58:26 +0530

    Added select image functionality

--- Image Recongition/image_recongition.py
+++ Image Recongition/image_recongition.py
@@ -2,14 +2,23 @@ import os
 import boto3
 
 
-def get_image(path):
-    image = open(path, 'rb')
+def get_image():
+    image_path = os.path.join(os.getcwd(),  'images')
+    images = os.listdir(image_path)
+    z = zip(images, range(len(images)))
+    print(tuple(z))
+    num = int(input('Enter the associated number: '))
+    if num < 0 or num > len(images):
+        raise ValueError
+    else:
+        image = os.path.join(image_path, images[num])
+    image = open(image, 'rb')
     return image
 
 
-def detect_label(image_path):
+def detect_label():
     client = boto3.client('rekognition', region_name='us-east-1')
-    image = get_image(image_path)
+    image = get_image()
     response = client.detect_labels(
         Image={"Bytes": image.read()}, MaxLabels=10)
     image.close()
@@ -17,6 +26,8 @@ def detect_label(image_path):
 
 
 if __name__ == '__main__':
-    response = detect_label('Image Recongition/images/got.jpg')
+    response = detect_label()
     for i in response['Labels']:
         print(f"{i['Name']} with {round(i['Confidence'],2)}% confidence ")
+    # print(os.getcwd())
+    # get_image()
