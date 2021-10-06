3d53160d6c6ae8b52a8d3a6028baa5a88713d3f8
Aaqil Shihab <aaqilsh@yahoo.com>
2021-10-06 20:42:38 +0530

    Added get_image method

--- Image Recongition/image_recongition.py
+++ Image Recongition/image_recongition.py
@@ -2,15 +2,21 @@ import os
 import boto3
 
 
-def detect_label():
+def get_image(path):
+    image = open(path, 'rb')
+    return image
+
+
+def detect_label(image_path):
     client = boto3.client('rekognition', region_name='us-east-1')
-    with open('Image Recongition/images/got.jpg', 'rb') as image:
-        response = client.detect_labels(
-            Image={"Bytes": image.read()}, MaxLabels=10)
+    image = get_image(image_path)
+    response = client.detect_labels(
+        Image={"Bytes": image.read()}, MaxLabels=10)
+    image.close()
     return response
 
 
 if __name__ == '__main__':
-    response = detect_label()
+    response = detect_label('Image Recongition/images/got.jpg')
     for i in response['Labels']:
         print(f"{i['Name']} with {round(i['Confidence'],2)}% confidence ")
